8f4130b250c241c22d1a94153fd04c1b1d410e0c
Denis <dbatrakov1@gmail.com>
2020-10-25 20:48:14 -0500

    2.0 final version

--- retirement.c
+++ retirement.c
@@ -6,18 +6,17 @@ struct _retire_info {
   double rate_of_return;
 };
 typedef struct _retire_info retire_info;
-
+double calculator (int startAge, double initial, retire_info status) {
+  for (int i = 1; i <= status.months; i++) {
+    printf("Age %3d month %2d you have $%.2f\n", startAge/12, startAge%12, initial);
+    initial = initial + (initial * status.rate_of_return) + status.contribution;
+    startAge++;
+}
+  return initial;
+}
 void retirement (int startAge, double initial, retire_info working, retire_info retired) {
-  for (int i = 1; i <= working.months; i++) {
-    printf("Age %3d month %2d you have $%.2f\n", startAge/12, startAge%12, initial);
-    initial = initial + (initial * working.rate_of_return) + working.contribution;
-    startAge++;
-  }
-  for (int i = 1; i <= retired.months; i++) {
-    printf("Age %3d month %2d you have $%.2f\n", startAge/12, startAge%12, initial);
-    initial = initial + (initial * retired.rate_of_return) + retired.contribution;
-    startAge++;
-  }
+  double finalBalance = calculator(startAge, initial, working);
+  calculator(816, finalBalance, retired);//816 =startAge(327) + working.months(489)
 }
 int main (void) {
   retire_info working;
